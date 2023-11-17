de6ed3fa650816d7145ca78e2b6f7c2cdc1fc8fd
RaniaMustafa17 <rania.mustafa1792@gmail.com>
2023-11-17 21:27:20 +0200

    main code

---
+++ main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include< calculator.h>
+
+int main()
+{
+    double result; 
+    double firstOperand =0;
+    double secondOperand =0;
+    int operation ; 
+ printf("enter the first operand : \n");
+    scanf("%lf", &firstOperand);
+    printf("enter the second operand : \n");
+    scanf("%lf", &secondOperand);
+    printf("0:addition , 1:subtraction \n 2: multiplication , 3:Division \n" );
+    printf("choose the number of operation \n" );
+    scanf("%d",&operation);
+    
+    switch(operation){
+        case 0:
+        addition(&result ,firstOperand ,secondOperand);
+        break;
+        case 1:
+        subtraction(&result ,firstOperand ,secondOperand);
+        break;
+        case 2:
+        multiplication(&result ,firstOperand ,secondOperand);
+        break;
+        case 3:
+        Division(&result ,firstOperand ,secondOperand);
+        break;
+        default:
+        printf("it's done");
+        
+    }
+
+    printf("%f", result);
+
+    return 0;
+}
