516edd03a068bffec18852c323508e7660283f0f
A-Talaat1 <151054344+A-Talaat1@users.noreply.github.com>
2023-11-17 22:56:24 +0200

    Delete calculator.h

--- calculator.h
+++
@@ -1,9 +0,0 @@
-#ifndef CALCULATOR_H_
-#define CALCULATOR_H_
-
-double addition(double *result, double operand1 ,double  operand2) ;
-double Division(double *result, double operand1 ,double  operand2) ;
-double multiplication(double *result, double operand1 ,double  operand2) ;
-double subtraction(double *result, double operand1 ,double  operand2) ;
-
-#endif CALCULATOR_H_
