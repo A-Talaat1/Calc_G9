03d60b2d96eb92fb550ca488e5587711750c8587
A-Talaat1 <151054344+A-Talaat1@users.noreply.github.com>
2023-11-17 22:53:43 +0200

    Update main.c

--- main.c
+++ main.c
@@ -66,12 +66,3 @@ double Division(double *result, double operand1 ,double  operand2)
 *result = operand1/operand2;
 return 1; //succeeded
 }
-double Division(double *result, double operand1 ,double  operand2) 
-{
-   if(operand2 == 0){
-        printf("it's forbidden");
-        return 0;
-    }
-*result = operand1/operand2;
-return 1; //succeeded
-}
